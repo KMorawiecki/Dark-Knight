5fe3d6b6d77b537893f8883c4ee8dbea68aebf23
Krzysiek <krzysiek.morawiecki@gmail.com>
2019-04-23 19:41:37 +0200

    Maze generation

---
+++ Scavenger_backup/Assets/_Complete-Game/Scripts/BoardManager.cs
@@ -0,0 +1,303 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 		//Allows us to use Lists.
+using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
+
+namespace Completed
+	
+{
+	
+	public class BoardManager : MonoBehaviour
+	{
+		// Using Serializable allows us to embed a class with sub properties in the inspector.
+		[Serializable]
+		public class Count
+		{
+			public int minimum; 			//Minimum value for our Count class.
+			public int maximum; 			//Maximum value for our Count class.
+			
+			
+			//Assignment constructor.
+			public Count (int min, int max)
+			{
+				minimum = min;
+				maximum = max;
+			}
+		}
+
+        public GameObject floorTile;									//All tiles
+        public GameObject leftWallTile;
+        public GameObject rightWallTile;
+        public GameObject topWallTile;
+        public GameObject botWallTile;
+        public GameObject topLeftWallTile;
+        public GameObject leftRightWallTile;
+        public GameObject leftBotWallTile;
+        public GameObject topRightWallTile;
+        public GameObject topBotWallTile;
+        public GameObject rightBotWallTile;
+        public GameObject leftTopRightWallTile;
+        public GameObject leftTopBotWallTile;
+        public GameObject leftRightBotWallTile;
+        public GameObject topRightBotWallTile;
+
+        public int columns = 16; 										//Number of columns in our game board.
+		public int rows = 16;											//Number of rows in our game board.
+		public Count wallCount = new Count (5, 9);						//Lower and upper limit for our random number of walls per level.
+		public Count foodCount = new Count (1, 5);						//Lower and upper limit for our random number of food items per level.
+		public GameObject exit;											//Prefab to spawn for exit.
+		public GameObject[] floorTiles;									//Array of floor prefabs.
+		public GameObject[] wallTiles;									//Array of wall prefabs.
+		public GameObject[] foodTiles;									//Array of food prefabs.
+		public GameObject[] enemyTiles;									//Array of enemy prefabs.
+		public GameObject[] outerWallTiles;								//Array of outer tile prefabs.
+		
+		private Transform boardHolder;									//A variable to store a reference to the transform of our Board object.
+		private List <Vector3> gridPositions = new List <Vector3> ();	//A list of possible locations to place tiles.
+        public List<List<Tile>> tiles = new List<List<Tile>>();
+
+
+        //Clears our list gridPositions and prepares it to generate a new board.
+        void InitialiseList ()
+		{
+			//Clear our list gridPositions.
+			gridPositions.Clear ();
+			
+			//Loop through x axis (columns).
+			for(int x = 1; x < columns-1; x++)
+			{
+				//Within each column, loop through y axis (rows).
+				for(int y = 1; y < rows-1; y++)
+				{
+					//At each index add a new Vector3 to our list with the x and y coordinates of that position.
+					gridPositions.Add (new Vector3(x, y, 0f));
+				}
+			}
+		}
+		
+		
+		//Sets up the outer walls and floor (background) of the game board.
+		void BoardSetup ()
+		{
+			//Instantiate Board and set boardHolder to its transform.
+			boardHolder = new GameObject ("Board").transform;
+
+            //clear list of all tiles from previous level
+            tiles.Clear();
+
+            for (int x = -1; x < columns + 1; x++)
+            {
+                List<Tile> newRow = new List<Tile>();
+                tiles.Add(newRow);
+
+                for (int y = -1; y < rows + 1; y++)
+                {
+                    //Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
+                    if (x == -1 || x == columns || y == -1 || y == rows)
+                    {
+                        GameObject toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+
+                        //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+                        GameObject instance =
+                            Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                        //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+                        instance.transform.SetParent(boardHolder);
+                    }
+                    else
+                    {
+                        GameObject go = new GameObject("Tile");
+                        Tile tile = go.AddComponent<Tile>();
+
+                        tile.SetPosition(x, y);
+                        tile.SetBoardMAnager(this);
+
+                        tiles[x].Add(tile);
+                    }
+                }
+            }
+        }
+
+        //tu dzieje sie magia
+        void PutWalls(int threshold, int start_hor, int start_ver)
+        {
+            //stop if map is full
+            if (rows < (int) (Math.Pow(2, threshold)))
+                return;
+            //put last wall randomly
+            else if(rows == (int)(Math.Pow(2, threshold)))
+            {
+                int lastWallPosition = Random.Range(0, 3);
+
+                switch(lastWallPosition)
+                {
+                    case 0:
+                        tiles[(int)(start_hor + columns / Math.Pow(2, threshold) - 1)][start_ver].right = true;
+                        break;
+                    case 1:
+                        tiles[(int)(start_hor + columns / Math.Pow(2, threshold) - 1)][start_ver + 1].right = true;
+                        break;
+                    case 2:
+                        tiles[start_hor][(int)(start_ver + rows / Math.Pow(2, threshold) - 1)].top = true;
+                        break;
+                    case 3:
+                        tiles[start_hor + 1][(int)(start_ver + rows / Math.Pow(2, threshold) - 1)].top = true;
+                        break;
+                }
+
+                return;
+            }
+
+            int hole = Random.Range(start_ver, (int) (start_ver + (rows / Math.Pow(2, threshold - 1)) - 1));
+
+            //put vertical wall in the middle
+            for (int i = start_ver; i < start_ver + rows / Math.Pow(2, threshold - 1); i++)
+            {
+                if (i != hole)
+                    tiles[(int) (start_hor + columns / Math.Pow(2, threshold) - 1)][i].right = true;
+                //TODO: jakos to zabezpieczyc przed skrajnymi
+                else
+                    tiles[(int)(start_hor + columns / Math.Pow(2, threshold) - 1)][i].right = false;
+            }
+
+            //if (columns < (int) (Math.Pow(2, threshold + 1)))
+            //    return;
+
+            hole = Random.Range(start_hor, (int)(start_hor + columns / Math.Pow(2, threshold) - 1));
+            //put horizontal wall on the left
+            for (int i = start_hor; i < start_hor + columns / Math.Pow(2, threshold); i++)
+            {
+                if (i != hole)
+                    tiles[i][(int)(start_ver + rows / Math.Pow(2, threshold) - 1)].top = true;
+                else
+                    tiles[i][(int)(start_ver + rows / Math.Pow(2, threshold) - 1)].top = false;
+            }
+
+            //invoke function recursively for left side
+            PutWalls(threshold + 1, start_hor, start_ver);
+            PutWalls(threshold + 1, start_hor, (int)(start_ver + rows / Math.Pow(2, threshold)));
+
+            hole = Random.Range((int)(start_hor + columns / Math.Pow(2, threshold)), (int)(start_hor + (columns / Math.Pow(2, threshold - 1)) -1));
+            //put horizontal wall on the right
+            for (int i = (int) (start_hor + columns / Math.Pow(2, threshold)); i < (int)(start_hor + columns / Math.Pow(2, threshold - 1)); i++)
+            {
+                if (i != hole)
+                    tiles[i][(int)(start_ver + rows / Math.Pow(2, threshold) - 1)].top = true;
+                else
+                    tiles[i][(int)(start_ver + rows / Math.Pow(2, threshold) - 1)].top = false;
+            }
+
+            //invoke function recursively for right side
+            PutWalls(threshold + 1, (int)(start_hor + (columns / Math.Pow(2, threshold))), start_ver);
+            PutWalls(threshold + 1, (int)(start_hor + (columns / Math.Pow(2, threshold))), (int)(start_ver + (rows / Math.Pow(2, threshold))));
+        }
+
+
+        //RandomPosition returns a random position from our list gridPositions.
+        Vector3 RandomPosition ()
+		{
+			//Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
+			int randomIndex = Random.Range (0, gridPositions.Count);
+			
+			//Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+			Vector3 randomPosition = gridPositions[randomIndex];
+			
+			//Remove the entry at randomIndex from the list so that it can't be re-used.
+			gridPositions.RemoveAt (randomIndex);
+			
+			//Return the randomly selected Vector3 position.
+			return randomPosition;
+		}
+		
+		
+		//LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
+		void LayoutObjectAtRandom (GameObject[] tileArray, int minimum, int maximum)
+		{
+			//Choose a random number of objects to instantiate within the minimum and maximum limits
+			int objectCount = Random.Range (minimum, maximum+1);
+			
+			//Instantiate objects until the randomly chosen limit objectCount is reached
+			for(int i = 0; i < objectCount; i++)
+			{
+				//Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
+				Vector3 randomPosition = RandomPosition();
+				
+				//Choose a random tile from tileArray and assign it to tileChoice
+				GameObject tileChoice = tileArray[Random.Range (0, tileArray.Length)];
+				
+				//Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
+				Instantiate(tileChoice, randomPosition, Quaternion.identity);
+			}
+		}
+
+        //initialise tiles
+        void LayoutFloor()
+        {
+            for (int row = 0; row < tiles.Count; row++)
+                for (int col = 0; col < tiles[row].Count; col++)
+                    tiles[row][col].Create();
+        }
+
+
+        //add walls on neighbor tiles
+        void UpdateTiles()
+        {
+            for (int row = 0; row < tiles.Count; row++)
+                for (int col = 0; col < tiles[row].Count; col++)
+                {
+                    if(row > 0)
+                        if (tiles[row - 1][col].right == true)
+                            tiles[row][col].left = true;
+                    if(col > 0)
+                        if (tiles[row][col - 1].top == true)
+                            tiles[row][col].bottom = true;
+                }
+        }
+
+        //add additional passages to simplify maze
+        void SimplifyMaze()
+        {
+            int passageCount = 32;
+
+            for(int i = 0; i < passageCount; i++)
+            {
+                int randomTileY = Random.Range(0, columns - 1);
+                int randomTileX = Random.Range(0, rows - 1);
+                //TODO: moznaby dodac sprawdzanie czy sie nie powtarzaja
+
+                tiles[randomTileX][randomTileY].right = false;
+                tiles[randomTileX][randomTileY].left = false;
+                tiles[randomTileX][randomTileY].top = false;
+                tiles[randomTileX][randomTileY].bottom = false;
+            }
+        }
+
+        //SetupScene initializes our level and calls the previous functions to lay out the game board
+        public void SetupScene (int level)
+		{
+			//Creates the outer walls and floor.
+			BoardSetup ();
+			
+			//Reset our list of gridpositions.
+			InitialiseList ();
+
+            PutWalls(1, 0, 0);
+            SimplifyMaze();
+            UpdateTiles();
+            LayoutFloor();
+
+            //Instantiate the exit tile in the upper right hand corner of our game board
+            Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
+		}
+
+        public Transform GetBoardHolder()
+        {
+            return boardHolder;
+        }
+
+        public Tile GetTile(int x, int y)
+        {
+            return tiles[x][y];
+        }
+    }
+}
---
+++ Scavenger_backup/Assets/_Complete-Game/Scripts/Tile.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Completed
+{
+    public class Tile : MonoBehaviour
+    {
+        public bool top = false;
+        public bool bottom = false;
+        public bool left = false;
+        public bool right = false;
+
+        private BoardManager board;
+        private Vector3 position;
+
+        public void SetPosition(int x, int y)
+        {
+            Vector3 pos = new Vector3(x, y, 0f);
+
+            position = pos;
+        }
+
+        public void SetBoardMAnager(BoardManager manager)
+        {
+            board = manager;
+        }
+
+        public int GetPositionX()
+        {
+            return (int) position.x;
+        }
+
+        public int GetPositionY()
+        {
+            return (int) position.y;
+        }
+
+        public void Create()
+        {
+            GameObject tile = new GameObject();
+
+            if (top == true && bottom == true && left == true && right == false)
+                tile = board.leftTopBotWallTile;
+            else if (top == true && bottom == true && left == false && right == false)
+                tile = board.topBotWallTile;
+            else if (top == true && bottom == false && left == false && right == false)
+                tile = board.topWallTile;
+            else if (top == false && bottom == true && left == false && right == false)
+                tile = board.botWallTile;
+            else if (top == false && bottom == false && left == true && right == false)
+                tile = board.leftWallTile;
+            else if (top == false && bottom == false && left == false && right == true)
+                tile = board.rightWallTile;
+            else if (top == true && bottom == false && left == true && right == false)
+                tile = board.topLeftWallTile;
+            else if (top == true && bottom == false && left == false && right == true)
+                tile = board.topRightWallTile;
+            else if (top == false && bottom == true && left == true && right == false)
+                tile = board.leftBotWallTile;
+            else if (top == false && bottom == true && left == false && right == true)
+                tile = board.rightBotWallTile;
+            else if (top == false && bottom == false && left == true && right == true)
+                tile = board.leftRightWallTile;
+            else if (top == true && bottom == true && left == false && right == true)
+                tile = board.topRightBotWallTile;
+            else if (top == true && bottom == false && left == true && right == true)
+                tile = board.leftTopRightWallTile;
+            else if (top == false && bottom == true && left == true && right == true)
+                tile = board.leftRightBotWallTile;
+            else if (top == false && bottom == false && left == false && right == false)
+                tile = board.floorTile;
+
+            //tile = board.floorTile;
+            GameObject instance =
+            Instantiate(tile, position, Quaternion.identity) as GameObject;
+
+            //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
+            instance.transform.SetParent(board.GetBoardHolder());
+        }
+    }
+}
